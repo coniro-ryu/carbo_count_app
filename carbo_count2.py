e16ba7941f7e1a08b1577f64c34f7fbeb170798a
coniro-ryu <5682pxeg@jcom.zaq.ne.jp>
2025-10-18 15:40:01 +0900

    Refactor carbo_count2.py for improved logic and inputs
    Refactored the carbohydrate counting logic to use dictionaries for better maintainability and added input fields for target blood sugar and insulin sensitivity.

--- carbo_count2.py
+++ carbo_count2.py
@@ -2,43 +2,50 @@ import math
 
 import streamlit as st
 
-st.title("糖質量(g)")
+st.title("簡易カーボカウント")
+
+# --- dict ---
+time_dict = {"朝": 30, "昼": 30, "夜": 40}
+food_dict = {
+    "米": {"weight": 160, "ratio": 0.4},
+    "パン": {"weight": 160, "ratio": 0.5},
+    "麺": {"weight": 160, "ratio": 0.2},
+}
+side_dict = {"油なし": 10, "油あり": 20}
 
 # --- 糖質比 ---
-st.subheader("**糖質比**")
-time = st.radio("時間帯を選んでください", ["朝", "昼", "夜"], horizontal=True)
-if time == "朝":
-    ratio = st.number_input("糖質比(朝)", value=30)
-elif time == "昼":
-    ratio = st.number_input("糖質比(昼)", value=40)
-else:
-    ratio = st.number_input("糖質比(夜)", value=40)
-carb_ratio = ratio
+st.subheader("糖質比")
+time = st.radio(
+    "時間帯を選んでください", list(time_dict.keys()), index=0, horizontal=True
+)
+time_val = st.number_input(f"糖質比({time})", value=time_dict[time])
 
 # --- 食事 ---
 st.subheader("食事")
-food = st.radio("主食", ["米", "パン", "麺"], horizontal=True)
-side = st.radio("副食", ["油なし", "油あり"], horizontal=True)
+food = st.radio("主食", list(food_dict.keys()), index=0, horizontal=True)
+side = st.radio("副食", list(side_dict.keys()), index=0, horizontal=True)
 
-if food == "米":
-    food_val = 160 * 0.4
-elif food == "パン":
-    food_val = 160 * 0.5
-elif food == "麺":
-    food_val = 160 * 0.2
-else:
-    food_val = 0
-
+food_val = st.number_input(food, value=food_dict[food]["ratio"])
 side_val = 10 if side == "油なし" else 20
 
-A = (food_val + side_val) / carb_ratio
+# --- 設定値 ---
+st.subheader("設定値")
+target = st.number_input("目標血糖値", value=100)
+isf = st.number_input("インスリン効果値", value=150)
+premeal = st.number_input("食前血糖値", value=0)
+
+S = food_val + side_val
+A = S / food_val
+B = (premeal - target) / isf
+total = A + B
+total_trunc = math.floor(total * 100) / 100
 
 st.markdown("### 結果")
 st.markdown(
     f"""
     <div style='font-size:22px; line-height:2'>
-        <strong>糖質量(g)：</strong> <span style='color:green; font-size:30px'><strong>{food_val + side_val:.0f}</strong></span><br>
-        <strong>インスリン量(U)：</strong> <span style='color:blue; font-size:30px'><strong>{A:.2f}</strong></span>
+        <strong>糖質量(g)：</strong> <span style='color:green; font-size:30px'><strong>{S:.0f}</strong></span><br>
+        <strong>注入量(U)：</strong> <span style='color:blue; font-size:30px'><strong>{total_trunc:.2f}</strong></span>
     </div>
     """,
     unsafe_allow_html=True,
